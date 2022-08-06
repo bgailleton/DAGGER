b3d37da9d8adf946a4529cca45668328bad5b7e1
bgailleton <boris.gailleton@gfz-potsdam.de>
2022-08-06 09:01:04 +0200

    hotfix to compile and renaming the example cpp to example.cpp

--- wrappers/cpp/example.cpp
+++ wrappers/cpp/example.cpp
@@ -8,13 +8,13 @@
 int main(int argc, char const *argv[])
 {
 	DAGGER::easyRand er = DAGGER::easyRand();
-	DAGGER::graph G(250000,8);
+	DAGGER::graph<double> G(250000,8);
 	DAGGER::D8connector<double> con(500, 500, 30, 30, 0, 0);
 	std::vector<double> topo(250000);
 	for(auto& v:topo) v=er.get();
 
 	G.init_graph(con);
-	std::vector<double> PPt = G.compute_graph<DAGGER::D8connector<double>, std::vector<double>, std::vector<double> >("carve",topo,con);
+	std::vector<double> PPt = G.compute_graph<DAGGER::D8connector<double>, std::vector<double>, std::vector<double> >("carve",topo,con,false);
 
 
 
